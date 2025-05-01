7b7b351ac86eefabbdf2164742d8869b71bfbfa7
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-05-01 09:35:28 +0200

    Update nn_structure.py

--- nn_structure.py
+++ nn_structure.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class AUTOENCODER(nn.Module):
-    def __init__(self, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons, 
-                 Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder, 
-                 Num_hidden_u_encoder, Ts, integrated_input_idxs=None):
+    def __init__(self, Num_meas, Num_inputs, Num_x_Obsv, Num_x_Neurons,
+                 Num_u_Obsv, Num_u_Neurons, Num_hidden_x_encoder,
+                 Num_hidden_u_encoder, Ts, integrated_input_idxs=None, Num_state_notU_affected = None):
 
         super(AUTOENCODER, self).__init__()
         self.num_meas = Num_meas
@@ -27,22 +27,22 @@ class AUTOENCODER(nn.Module):
                 self.x_koopman_fixed.weight[i, i] = 1.0
         self.x_koopman_fixed.weight.requires_grad = False
 
-        self.x_koopman_train = nn.Linear(K, K - M, bias=False)     
+        self.x_koopman_train = nn.Linear(K, K - M, bias=False)
 
         #------------------------
 
         self.u_Encoder_In = nn.Linear(Num_meas + Num_inputs, Num_u_Neurons)
         self.u_encoder_hidden = nn.ModuleList([nn.Linear(Num_u_Neurons, Num_u_Neurons) for _ in range(Num_hidden_u_encoder)])
         self.u_Encoder_out = nn.Linear(Num_u_Neurons, Num_u_Obsv)
- 
+
         #------------------------
 
-        self.u_koopman_fixed = nn.Linear(Num_u_Obsv + Num_inputs, Num_inputs, bias=False)
+        self.u_koopman_fixed = nn.Linear(Num_u_Obsv + Num_inputs, Num_state_notU_affected, bias=False)
         with torch.no_grad():
             self.u_koopman_fixed.weight.zero_()
         self.u_koopman_fixed.weight.requires_grad = False
 
-        self.u_koopman_train = nn.Linear(Num_u_Obsv + Num_inputs, Num_x_Obsv + Num_meas - Num_inputs, bias=False)
+        self.u_koopman_train = nn.Linear(Num_u_Obsv + Num_inputs, Num_x_Obsv + Num_meas - Num_state_notU_affected, bias=False)
         #------------------------
 
         self._init_weights()
@@ -64,8 +64,8 @@ class AUTOENCODER(nn.Module):
         return x
 
     def x_Koopman_op(self, x):
-        x_fixed = self.x_koopman_fixed(x)    # → [batch, M]
-        x_train = self.x_koopman_train(x)    # → [batch, K-M]
+        x_fixed = self.x_koopman_fixed(x)
+        x_train = self.x_koopman_train(x)
         return torch.cat((x_fixed, x_train), dim=1)
 
     def u_Encoder(self, x):
